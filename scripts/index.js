a117181d282f4b8f608a0dc4bf13640627661c48
maitymohar <140618645+maitymohar@users.noreply.github.com>
2024-06-13 18:48:56 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jan 29, 2020',
-  name: 'Darlene'
+  birthdate: 'Jul 4, 2024',
+  name: 'Mimo'
 };
 
 function hideEverything() {
